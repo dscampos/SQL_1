0f528f0ff20a5999f7ccf940cab7149a6e516fef
dscampos <45672635+dscampos@users.noreply.github.com>
2021-11-10 10:05:35 -0300

    Add files via upload

---
+++ empresa1_consulta6.sql
@@ -0,0 +1,6 @@
+/* Quais são os códigos e a descricao dos porjetos ordenado pela descricao 
+em ordem crecente */
+
+select  CodigoProjeto, DescricaoProjeto from Projetos
+
+order by  DescricaoProjeto ASC;
